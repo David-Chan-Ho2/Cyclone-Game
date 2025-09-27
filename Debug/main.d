b9d1740ce5e61b1870ec02846eeb7bed7d5c0eb5
David-Chan-Ho2 <dho12@charlotte.edu>
2025-09-27 10:13:41 -0400

    add files

---
+++ Debug/main.d
@@ -0,0 +1,39 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Applications/ti/ccstheia131/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: /Applications/ti/ccstheia131/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: /Applications/ti/ccstheia131/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /Applications/ti/ccstheia131/ccs/ccs_base/msp430/include/msp430g2553.h
+main.obj: /Applications/ti/ccstheia131/ccs/ccs_base/msp430/include/in430.h
+main.obj: /Applications/ti/ccstheia131/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+main.obj: /Applications/ti/ccstheia131/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+main.obj: /Applications/ti/ccstheia131/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+main.obj: ../main.h
+main.obj: ../led.h
+main.obj: ../button.h
+
+../main.c:
+
+/Applications/ti/ccstheia131/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+/Applications/ti/ccstheia131/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+/Applications/ti/ccstheia131/ccs/ccs_base/msp430/include/msp430.h:
+
+/Applications/ti/ccstheia131/ccs/ccs_base/msp430/include/msp430g2553.h:
+
+/Applications/ti/ccstheia131/ccs/ccs_base/msp430/include/in430.h:
+
+/Applications/ti/ccstheia131/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
+/Applications/ti/ccstheia131/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+/Applications/ti/ccstheia131/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+../main.h:
+
+../led.h:
+
+../button.h:
+
---
+++ led.h
@@ -0,0 +1,23 @@
+#ifndef LED_H
+#define LED_H
+
+#include <msp430.h>
+
+#define LED_MASK_ALL (BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5 | BIT6)
+#define LED_MASK_ODD (BIT1 | BIT5)
+#define LED_MASK_EVEN (LED_MASK_ALL & ~LED_MASK_ODD)
+#define LED_MASK_WIN BIT3
+#define LED_MASK_NO_WIN (LED_MASK_ALL & ~LED_MASK_WIN)
+
+#define LED_LAST 6
+#define LED_FIRST 0
+#define LED_WIN 3
+#define LED_DIR_REG P2DIR
+#define LED_OUT_REG P2OUT
+#define LED_SEL_REG P2SEL
+#define LED_SEL2_REG P2SEL2
+
+static inline void allLedOff();
+static inline void updateLED(int currentLED, int previousLED);
+
+#endif
